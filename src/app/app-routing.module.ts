ed1356710259f742bf4b16c73d499887137d3393
RitaMonreal <A326347@alumnos.uaslp.mx>
2023-11-25 18:53:59 -0600

    se agregó el formulario para agregar el articulo

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,7 @@ import { CrearCuentaComponent } from './crear-cuenta/crear-cuenta.component';
 import { NotFoundComponent } from './not-found/not-found.component';
 import { ContenidoPage } from './contenido/contenido.pages';
 import { DetallesArticuloComponent } from './contenido/components/detalles-articulo/detalles-articulo.component';
+import { FormularioNuevaPublicacionComponent } from './formulario-nueva-publicacion/formulario-nueva-publicacion.component';
 
 const routes: Routes = [
   {path: "iniciar_sesion", component: IniciarSesionComponent},
@@ -12,6 +13,7 @@ const routes: Routes = [
   { path: "pagina_principal", component: ContenidoPage },
   {path: "detalles-articulo/:id", component: DetallesArticuloComponent},
   {path: "", redirectTo: "pagina_principal", pathMatch: "full"},
+  {path: "nueva_publicacion", component: FormularioNuevaPublicacionComponent},
   {path: "not-found", component: NotFoundComponent},
   {path: "**", redirectTo: "not-found", pathMatch: "full"},
  
